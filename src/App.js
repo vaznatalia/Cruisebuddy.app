650255864bd349371c89e0ee14f258c054065df3
vaznatalia <natalialuizavaz@gmail.com>
2019-11-23 16:06:05 -0500

    api-link

--- src/App.js
+++ src/App.js
@@ -1,45 +1,43 @@
-// import libraries
-
-import React, { Component } from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-
-
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { setDataFrom, ENDPOINTS } from "./lib/clientUtil.js";
 // import pages
-import Home from './pages/Home'
-import Search from './pages/Search'
-import Ship from './pages/Ship'
-import About from './pages/About'
-
-
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Ship from "./pages/Ship";
+import About from "./pages/About";
+// need to impor the utility file
 // import components
-import {NavigationBar} from './components/NavigationBar'
-
-
-
+import {NavigationBar} from "./components/NavigationBar";
 // import CSS
-
-import './styles/global.css'
-import './styles/home.css'
-import './styles/search.css'
-import './styles/ship.css'
-import './styles/about.css'
-
-
+import "./styles/global.css";
+import "./styles/home.css";
+import "./styles/search.css";
+import "./styles/ship.css";
+import "./styles/about.css";
 class App extends Component {
+  k_cruise_lines = 'cruise_lines'
+  k_ship = "ships"
+  state = {[this.k_ship] : []}
   render() {
     return (
-      <BrowserRouter>
-      <NavigationBar/>
-      <Switch>
-        <Route exact path="/" component={Home}/>
-      
-        <Route exact path="/search" component={Search} />  
-        <Route path="/ship/:id" component={Ship} />
-        <Route exact path="/about" component={About} />      
-      </Switch>
-    </BrowserRouter>
-    )
+      <>
+        <BrowserRouter>
+          <NavigationBar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/search" component={Search} />
+            <Route path="/ship/:id" component={Ship} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </BrowserRouter>
+      </>
+    );
+  }
+  componentDidMount() {
+    setDataFrom(this.k_cruise_lines, ENDPOINTS[this.k_cruise_lines], this)
+    setDataFrom(this.k_ship, ENDPOINTS[this.k_ship], this);
   }
 }
-
 export default App;
