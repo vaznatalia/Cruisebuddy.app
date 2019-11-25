b082cb83b14133cb09d6794fabe46add6b4d2cb6
Daniel Ulerio <daniel.s.ulerio@gmail.com>
2019-11-25 18:28:35 -0500

    saving my work

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -24,40 +24,20 @@ class Home extends Component {
   render(){
     const { query, destination, port } = this.state;
     return(
-      <>
-    <NavigationBar />
-  <div  className="hero-div">
-    <div className="container">
-      <form onSubmit={this.handleSubmit}>
-      <div className="form-group">
- 
-      <label for="home-input">Find</label>
-      <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" value={query} onChange={this.handleQueryChange} />
+        <>
+    <div  className="hero-div">
+      <div className="container">
+        <form onSubmit={this.handleSubmit}>
+        <div className="form-group">
+   
+        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Search" value={query} onChange={this.handleQueryChange} />
+        </div>
+        </form>
       </div>
-      </form>
     </div>
-
-    <select className="browser-default custom-select" value={destination} onChange={this.handleFilterChange('destination')}>
-      <option selected>Destination</option>
-      <option value="d-1">D-One</option>
-      <option value="d-2">D-Two</option>
-      <option value="d-3">D-Three</option>
-    </select>
-    <select className="browser-default custom-select" value={port} onChange={this.handleFilterChange('port')}>
-      <option selected>Port</option>
-      <option value="p-1">P-One</option>
-      <option value="p-2">P-Two</option>
-      <option value="p-3">P-Three</option>
-    </select>
-    <select className="browser-default custom-select">
-      <option selected>Any Departure</option>
-      <option value="1">One</option>
-      <option value="2">Two</option>
-      <option value="3">Three</option>
-    </select>
-
-  </div>
-  </>
+  
+    
+    </>
 )
 }
 }
