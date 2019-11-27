a0cde37d623f4d34497eab1b2d056c0a83accda2
vaznatalia <natalialuizavaz@gmail.com>
2019-11-27 16:24:10 -0500

    some file modification

--- src/components/Star.js
+++ src/components/Star.js
@@ -2,8 +2,5 @@ import React from 'react'
 
 
 
-const Star = ({ selected = false, onClick = f => f }) => (
-    <div className={selected ? "star selected" : "star"} onClick={onClick} />
-  );
-  render(<StarRating totalStars={5} />, document.getElementById("root"));
+const Star = ({ }) => (
 
