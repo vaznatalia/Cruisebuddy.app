6556dc84b3276f456281c7f58c5bf78c32c14647
vaznatalia <natalialuizavaz@gmail.com>
2019-11-27 19:58:14 -0500

    all uptodate

---
+++ src/components/Description.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import Star from '../components/Star'
+
+export const Description = ({ description }) => {
+    return (
+
+        <div>
+            <h1> Carnival Horizon</h1>
+            <Star />
+            <div>Carnival Horizon – which set sail in April 2018 –  is one of the fleet's newer ships. 
+                It accommodates 1,450 crew members and a maximum of 5,101 passengers,
+                 and it offers a plethora of amenities for travelers of all ages. 
+                 Children can ride the SkyRide suspended bike, play at the Dr. Seuss-themed water park and 
+                 climb the ropes course, while parents can lounge on an adults-only deck and pamper 
+                 themselves at the Cloud 9 Spa. At night, the whole family can enjoy 
+                live performances and lip-syncing competitions.</div>
+
+
+                <h1>Reviews</h1>
+                
+
+        </div>
+
+    )
+}
--- src/pages/Review.js
+++ src/pages/Review.js
@@ -42,22 +42,7 @@ class Review extends React.Component{
       <h1>We’d love to hear your opinion. Your feedback helps 
           fellow cruisers choose the best cruise for their needs.  
           Thank you. </h1> 
-
-import { Form, Button } from 'react-bootstrap'
-import { StarRating } from '../components/StarRating';
-
-
-
-class Review extends React.Component {
-
-
-  render() {
-    return (
-      <div>
-        <div className="reviews-img">
-          <h1>We’d love to hear your opinion. Your feedback helps
-              fellow cruisers choose the best cruise for their needs.
-          Thank you. </h1>
+    
 
           <Form className="review-input">
             <div controlId="formGrid">
@@ -76,66 +61,23 @@ class Review extends React.Component {
           </Form>
 
 
-
-
-
-
-
-
         </div>
         <div className="main-review-page">
 
           <label>Choose Overal Rating</label>
 
           <StarRating value={averageRating} />
-            <div className="c-2"> 
+           
             {categories.map(({ value, label }) => (
-              <>
+              <div>
                 <label>{label}</label>
                 <StarRating category={value} value={this.state[value]} handleRating={this.handleRatingSelection(value)} />
-              </>
+              </div>
               ))}
-
-          <StarRating title="" />
-          <div className="c-2">
-            <div className="div11">
-              <label>Lodging</label>
-              <StarRating title="" />
-              <label>Dinig</label>
-              <StarRating title="" />
-              <label>Public Areas</label>
-              <StarRating title="" />
-              <label>Enterteriment</label>
-              <StarRating title="" />
-
-            </div>
-            <div className="div22">
-              <label>Fitness/Recreation  </label>
-              <StarRating title="" />
-              <label>Excursions</label>
-              <StarRating title="" />
-              <label>Service</label>
-              <StarRating title="" />
-              <label>Value</label>
-              <StarRating title="" />
-            </div>
-          </div>
-
-          <div class="form-group-">
-            <h1 >Write your review</h1>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="10"></textarea>
-          </div>
-          <button type="submit" class="btn btn-primary btn-lg">Post Review</button>
-        </div>
-      </div>
-
-
-
-
+             </div>
+         </div>
     )
+    
   }
 }
-
-
-
 export default Review
