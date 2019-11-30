2f4a8c62f1e701ea083e0185320d473fe3493339
vaznatalia <natalialuizavaz@gmail.com>
2019-11-30 16:23:37 -0500

    set  navbar to  change color when scroll and modified homepage

--- src/components/MyDropzone.js
+++ src/components/MyDropzone.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import axios from 'axios'
-
+import upload from '../images/upload.png'
 
 class  MyDropzone extends React.Component {
   state ={
@@ -20,27 +20,18 @@ class  MyDropzone extends React.Component {
     axios.post('http://localhost:4000", //&& "http://zip.thruhere.net:3001', formData)
   }
   
-    //  fileUploadHandler = ()=>{ 
-    //    const  fd = new FormData()
-    //    fd.append('image', this.state.selectedFile, this.state.selectedFile.name)
-    //    axios.post('http://localhost:4000", //&& "http://zip.thruhere.net:3001', fd, {
-    //    onUploadProgress:  progressEvent => {
-    //    console.log ('Upload Progress:' + Math.round (progressEvent.loaded / progressEvent.total* 100) + '% ')
-    //    }
-    //  })
-    //    .then(res => {
-    //    console.log(res)
-    //  })
-    //  }
   
   render() {
       return (
 
-        <div className="box" method="post"  >
+  
+       <div className="box" method="post"  >
+    
+         
         <input  style={{display: 'none'}} className="box" type="file"  onChange={this.fileSelectedHandler } 
         ref={fileInput => this.fileInput = fileInput}/>
-        <button onClick={() => this.fileInput.click()} >Pick File</button>
-         <button onClick={this.UploadHandler}>Upload</button>
+        <button  class="btn btn-light" onClick={() => this.fileInput.click()} >Attach photo</button>
+         <button class="btn btn-light" onClick={this.UploadHandler}>Upload</button>
          
         </div>
       )
