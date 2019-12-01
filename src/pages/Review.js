9abbe21b05680ed5222f623c61f24b077b0758b9
Chuniversity <contact@chunis.org>
2019-11-30 19:04:20 -0500

    fixes to last update

--- src/pages/Review.js
+++ src/pages/Review.js
@@ -5,11 +5,9 @@ import { AuthContext } from '../context/AuthContext';
 import '../styles/Review.css'
 import StarRating from '../components/StarRating';
 import MyDropzone from '../components/MyDropzone'
-
 import ReactDOM from "react-dom";
 import { Container, Header, List } from "semantic-ui-react";
 
-import CommentExample from "../components/CommentExample";
 
 const CATEGORIES = [
   { label: 'Fitnesss Rating', value: 'fitnessRating' },
